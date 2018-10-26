288e6ffff8fc871b416325509925865a6c3aff19
mayankraghuvanshi <mayank6388@gmail.com>
2018-10-26 23:14:31 +0530

    intinal commit

---
+++ translate.py
@@ -0,0 +1,7 @@
+from watson_developer_cloud import LanguageTranslatorV2
+#Credencial added from IBM cloud.
+language_translator = LanguageTranslatorV2(
+    username='{5bb173e4-db59-4b54-b55b-f9b34b934c46}',
+    password='{0TtwvNoKnSgb}',
+    url='{https://gateway.watsonplatform.net/language-translator/api}'
+)
