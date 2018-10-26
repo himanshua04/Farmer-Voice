67dc154219057ba72f09d39b9078e2ec85c44b75
mayankraghuvanshi <mayank6388@gmail.com>
2018-10-27 02:07:47 +0530

    access denied!

--- trans.py
+++ trans.py
@@ -1,9 +1,22 @@
-from watson_developer_cloud import LanguageTranslatorV2
-#Credencial added from IBM cloud.
-language_translator = LanguageTranslatorV2(
-	#plz don't share this :)
+#LanguageTranslatorV2 is not no longer exist in watson_developer_cloud :(
+from watson_developer_cloud import LanguageTranslatorV3
+
+iimport json
+language_translator = LanguageTranslatorV3(
+    # Don't default to the current date. Instead, specify a date that matches a 
+    # version that is compatible with your app, and don't change it until your app
+    # is ready for a later version.
+    version='{2018-05-01}',
     username='{5bb173e4-db59-4b54-b55b-f9b34b934c46}',
     password='{0TtwvNoKnSgb}',
     url='{https://gateway.watsonplatform.net/language-translator/api}'
 )
-#LanguageTranslatorV2 is not no longer exist in watson_developer_cloud :(
+#Service end-pt
+language_translator.set_url('https://gateway-wdc.watsonplatform.net/language-translator/api')
+
+language_translator.set_default_headers({'x-watson-learning-opt-out': "true"})
+
+translation = language_translator.translate(
+    text='Hello',
+    model_id='en-es').get_result()
+print(json.dumps(translation, indent=2, ensure_ascii=False))
