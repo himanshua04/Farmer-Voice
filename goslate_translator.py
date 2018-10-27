6c6b30f5fe63dc8e0fbf6979b7144e99106f966b
mayankraghuvanshi <mayank6388@gmail.com>
2018-10-27 16:44:03 +0530

    unusefull files removed.

--- goslate_translator.py
+++
@@ -1,9 +0,0 @@
-#pip install goslate
-import goslate
-
-text = "Hello World"
-
-gs = goslate.Goslate()
-translatedText = gs.translate(text,'hi')
-
-print(translatedText)
--- translate.py
+++
@@ -1,8 +0,0 @@
-from watson_developer_cloud import LanguageTranslatorV2
-#Credencial added from IBM cloud.
-language_translator = LanguageTranslatorV2(
-	#plz don't share this :)
-    username='{5bb173e4-db59-4b54-b55b-f9b34b934c46}',
-    password='{0TtwvNoKnSgb}',
-    url='{https://gateway.watsonplatform.net/language-translator/api}'
-)
